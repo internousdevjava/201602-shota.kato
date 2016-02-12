7aed874227e6616acf303f0ec8ded208c5af48a6
Shota Kato <skato042785@gmail.com>
2016-02-12 16:53:12 +0900

    はじめてのプログラム

---
+++ src/HolloWorld.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
+public class HolloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+			System.out.println("HelloWprld!");
+
+	}
+
+}
